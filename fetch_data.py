faf57e58c4fb7b98f7b1ffd07f0871c86a7e6e0c
aedwardg <aedwardg1@gmail.com>
2020-02-21 12:15:14 -0800

    Updated to fix problems

--- fetch_data.py
+++ fetch_data.py
@@ -5,56 +5,94 @@ by aedwardg
 
 from bs4 import BeautifulSoup
 import requests
+import time 
+import random 
 
 # Parses html from page_link and saves all links from page to all_links
 def get_links(page_link):
     page_response = requests.get(page_link, timeout=5)
-    full_html = BeautifulSoup(page_response.content, "html.parser")
-    all_links = full_html.find_all("a")
+    full_html = BeautifulSoup(page_response.content, 'html.parser')
+    all_links = full_html.find_all('a')
     return all_links
 
 # removes unnecessary page links and duplicates
 def get_song_links(links):
     song_links = []
-    for link in range(28, (len(links) - 7)):
-        song_links.append(links[link].get("href"))
+    # As of Feb. 2020 there are 28 links in the navbar, 3 share links (FB, Twitter, Mail), and 8 links at the bottom of the page
+    # This number may change and need to be updated
+    for link in range(31, (len(links) - 8)):
+        song_links.append(links[link].get('href'))
     song_links = list(set(song_links))
     for entry in song_links:
-        if entry == "#" or entry == None:
+        if entry == '#' or entry == None:
             song_links.remove(entry)
         else:
             continue
+    print('Retrieved links for {} songs'.format(len(song_links)))
     return song_links
 
-'''
-fix_links was necessary because of the html in the links on an artist's page.
-pulling the links returned many that were '../d/drake/song_name'
-'''
+
+"""
+`fix_links` was necessary due to relative links in the html.
+E.g., '../d/drake/song_name'
+"""
 def fix_links(links):
+    print('Fixing song links')
     for x in range(0, len(links)):
-        if list(links[x])[0] == "." and list(links[x])[1] == ".":
-            links[x] = "https://www.azlyrics.com" + "".join(list(links[x])[2:])
+        if list(links[x])[0] == '.' and list(links[x])[1] == '.':
+            links[x] = 'https://www.azlyrics.com' + ''.join(list(links[x])[2:])
     return links
 
-'''
-returns lyrics in list of phrases
-choose number of links to scrape at your own discretion.
-I have successfully scraped up to 50 before they block my ip address,
-but 25 is a safer bet.
-'''
-def get_lyrics(urls):
+
+"""
+Returns lyrics in list of phrases.  
+Pauses for 10 to 20 seconds between scraping each page of lyrics.
+May need to update cleaning loop in the future if page changes.
+"""
+def get_lyrics(urls, max_songs=None, sleep_min=10, sleep_max=20):
+    start_time = time.time()
     all_text = []
-    for link in urls[:25]:
-        url_response = requests.get(link, timeout=5)
-        url_html = BeautifulSoup(url_response.content, "html.parser")
-        for script in url_html(["head", "script", "style","span", "a", "h2", "small"]):
+
+    link_requests = 0
+
+    if max_songs is not None:
+        num_songs = max_songs
+    else:
+        num_songs = len(urls)
+
+    for link in urls[:num_songs]:
+        print('Scraping: {}'.format(link))
+        
+        try:
+            url_response = requests.get(link, timeout=10)
+        except:
+            print('Read timout')
+            pass
+        
+        # Removes unnecessary text on page
+        url_html = BeautifulSoup(url_response.content, 'html.parser')
+        for script in url_html(['head', 'script', 'style', 'span', 'a', 'h1', 'h2', 'small', 'b', 'i']):
             script.extract()
+        
+        # Removes informational blocks after lyrics
+        for tag in url_html.find_all('div', class_='panel'):
+            tag.extract()
+
         for string in url_html.stripped_strings:
             all_text.append(string)
+
+        current_time = time.time()
+        elapsed_time = current_time - start_time
+        link_requests += 1
+
+        print('Songs scraped: {0} of {1}'.format(link_requests, num_songs))
+        print('Elapased Time: {} minutes'.format(elapsed_time/60))
+        print('Pausing...')    
+        time.sleep(random.uniform(sleep_min, sleep_max))  
     return all_text
 
-# Writes all the lyrics to a file for that artist
+"""Writes all the lyrics to a file for that artist"""
 def write_to_file(artist_file, lyric_list):
-    with open(artist_file, "w") as f:
+    with open(artist_file, 'w') as f:
         for phrase in lyric_list:
-            f.write(phrase + "\n")
+            f.write(phrase + '\n')
--- run.py
+++ run.py
@@ -2,38 +2,40 @@ from fetch_data import *
 from markov_python.cc_markov import MarkovChain
 
 # Insert the artist page link from azlyrics.com
-page_link = "https://www.azlyrics.com/a/aesoprock.html"
+page_link = 'https://www.azlyrics.com/b/billieeilish.html'
 # Insert name of text file for lyric all lyrics
-file = "aesop.txt"
+full_file = 'billie.txt'
 # Insert name of output file
-output = "output.txt"
+output = 'output.txt'
+# Insert limit on number of songs or None to scrape all songs
+max_songs = 25
 
-
-def save_lyrics(url, doc):
+def save_lyrics(url, doc, max_songs=None):
     all_links = get_links(url)
     songs = get_song_links(all_links)
     fix_links(songs)
-    lyrics = get_lyrics(songs)
+    lyrics = get_lyrics(songs, max_songs=max_songs)
     write_to_file(doc, lyrics)
 
-
-
+ 
 def write_song(doc1, doc2):
     mc = MarkovChain()
     verse = 1
     mc.add_file(doc1)
-    with open(doc2, "w") as f:
+    with open(doc2, 'w') as f:
         while verse < 5:
-            f.write(("Verse %s" + "\n") % (verse))
+            f.write(('Verse %s' + '\n') % (verse))
             for x in range(4):
                 line = mc.generate_text()
-                join_line = " ".join(line)
-                f.write(join_line.capitalize() + "\n")
+                join_line = ' '.join(line)
+                f.write(join_line.capitalize() + '\n')
+            f.write('\n')
             verse += 1
 
 
+def run_all(artist=page_link, lyric_file=full_file, output_doc=output, max_songs=max_songs):
+    save_lyrics(page_link, full_file, max_songs=max_songs)
+    write_song(full_file, output)
 
-
-
-#save_lyrics(page_link, file)
-write_song(file, output)
+if __name__ == '__main__':
+    run_all()
